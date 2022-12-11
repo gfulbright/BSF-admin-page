ad1b9fc14643467130ab40d5ccdb067ac11ef8b6
Alaris-Chrine <icant@CHARIS-LAPTOP.(none)>
2022-12-11 19:11:56 +0000

    Start of Tickets component and altered login/out sequence

---
+++ client/src/components/Tickets.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios'
+let hidden = true;
+
+
+
+const Tickets = () => {
+	
+	if(!hidden)
+	{
+		return(
+			<div>
+				<h1>Tickets!</h1>
+			</div>
+		)
+	}
+}
+
+export default Tickets;
